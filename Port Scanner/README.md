ad7824b26ebc31bbdc38fd1497e6481d1609ecae
kartikbhushan <kb.mg.sg@gmail.com>
2023-01-07 19:05:06 +0530

    Port Scanner

---
+++ Port Scanner/README.md
@@ -0,0 +1,22 @@
+## Port Scanner 
+
+This port Scanner utilizes the socket connection functionality to determine range of open ports for the given IP.
+
+The output of the scan is written into a text file called - *open_ports.txt*
+
+## Usuage 
+
+**Specify the IP and the port range you want to conduct the scan for in the python code.**
+
+```
+python3 port_scanner.py
+```
+
+## Output - 
+
+```
+Scanning Ports from range 1 - 100
+Default Socket timeout is set to 0.1
+ALL THE OPEN PORTS ARE: []
+Results in  - open_ports
+```
---
+++ Port Scanner/port_scanner.py
@@ -0,0 +1,44 @@
+import socket
+
+class Scanner:
+    def __init__(self , ip):
+        self.ip = ip
+        self.open_ports = []
+        self.timeout = 0.1
+
+    def __repr__(self) -> str:
+        return 'Scanner: {}'.format(self.ip)
+
+    def scan(self, lowerport , upperport):
+        print("Scanning Ports from range " + str(lowerport) + " - " + str(upperport))
+        print("Default Socket timeout is set to {}".format(self.timeout))
+        for port in range(lowerport , upperport+1):
+            if self.is_open(port):
+                self.open_ports.append(port)
+
+    def is_open(self,port):
+        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        s.settimeout(self.timeout)
+        result = s.connect_ex((self.ip , port))
+        # print('port {}:    {}'.format(port, result))
+        s.close()
+        return result == 0
+
+    def write(self, filepath):
+        print("Results in  - open_ports")
+        openports = map(str,self.open_ports)
+        with open(filepath , 'w') as f:
+            f.write("\n".join(openports))
+
+
+def main():
+    ip = '192.168.0.107'
+    lowerport = 1
+    upperport = 100
+    scanner = Scanner(ip)
+    scanner.scan(lowerport , upperport)
+    print("ALL THE OPEN PORTS ARE: {}".format(scanner.open_ports))
+    scanner.write("./open_ports")
+
+if __name__ == '__main__':
+    main()
